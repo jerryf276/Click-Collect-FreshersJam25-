f35485f14448cbc2cca814c2ed2e2f80363a4844
Tstebbs <2201393@uad.ac.uk>
2025-09-26 20:33:50 +0100

    Added: button menu

--- freshers-jam-game/GameManager.cs
+++ freshers-jam-game/GameManager.cs
@@ -3,4 +3,51 @@ using System;
 
 public partial class GameManager : Node2D
 {
+    enum SceneState
+    {
+        MAIN_MENU,
+        IN_GAME
+    }
+
+    enum Players
+    {
+        Solo,
+        Duo
+    }
+
+    bool isPaused;
+
+    Node2D currentScene;
+
+    string MAIN_MENU_SCENE = "res://MainMenu.tscn";
+   // string GAME_SCENE = "res://Game.tscn";
+
+
+
+    public override void _Ready()
+    {
+        DisplayServer.WindowSetTitle("Click & collect");
+        
+
+    }
+
+
+    public override void _Process(double delta)
+    {
+
+
+    }
+
+    static public void OnSoloStart()
+    {
+        
+    }
+
+
+    static public void OnDuoStart()
+    {
+
+    }
+
+
 }
