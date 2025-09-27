21471d522efa6920ce66be14ec735cc6631ed6f1
Jerry <55096594+jerryf276@users.noreply.github.com>
2025-09-27 12:00:10 +0100

    commit: player speeds adjusted

--- freshers-jam-game/Player.cs
+++ freshers-jam-game/Player.cs
@@ -3,72 +3,73 @@ using System;
 
 public partial class Player : Node2D
 {
-    private float playerSpeed = 250;
-    [Export] private int playerNumber = 1;
-    [Export] private CharacterBody2D player;
-    private Vector2 lastDirection = new Vector2(1, 0);
-    [Export] private int inventorySize = 0;
-    private int maxInventoryCapacity = 5;
-    public override void _Ready()
-    {
+	private float playerSpeed = 250;
+	[Export] private float maxPlayerSpeed = 250;
+	[Export] private int playerNumber = 1;
+	[Export] private CharacterBody2D player;
+	private Vector2 lastDirection = new Vector2(1, 0);
+	[Export] private int storedInventory = 0;
+	private int maxInventoryCapacity = 5;
+	public override void _Ready()
+	{
 
-    }
-    public override void _PhysicsProcess(double delta)
-    {
-        //if (playerNumber == 1)
-        //{
-            var direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-        //}
-        if (playerNumber == 2)
-        {
-            direction = Input.GetVector("move_left_p2", "move_right_p2", "move_up_p2", "move_down_p2");   
-        }
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		//if (playerNumber == 1)
+		//{
+			var direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		//}
+		if (playerNumber == 2)
+		{
+			direction = Input.GetVector("move_left_p2", "move_right_p2", "move_up_p2", "move_down_p2");   
+		}
 
-        switch (inventorySize)
-        {
-            case 0:
-                playerSpeed = 250;
-                break;
-            case 1:
-                playerSpeed = 200;
-                break;
-            case 2:
-                playerSpeed = 160;
-                break;
-            case 3:
-                playerSpeed = 120;
-                break;
-            case 4:
-                playerSpeed = 80;
-                break;
-            case 5:
-                playerSpeed = 40;
-                break;
-        }
+		switch (storedInventory)
+		{
+			case 0:
+				playerSpeed = maxPlayerSpeed;
+				break;
+			case 1:
+				playerSpeed = maxPlayerSpeed * 0.85f;
+				break;
+			case 2:
+				playerSpeed = maxPlayerSpeed * 0.7f;
+				break;
+			case 3:
+				playerSpeed = maxPlayerSpeed * 0.55f;
+				break;
+			case 4:
+				playerSpeed = maxPlayerSpeed * 0.4f;
+				break;
+			case 5:
+				playerSpeed = maxPlayerSpeed * 0.25f;
+				break;
+		}
 
-        //if (inventorySize == 0)
-        //{
-        //    playerSpeed = 100;
-        //}
-        //else if (inventorySize == 1)
-        //{
-        //    playerSpeed = 80;
-        //}
+		//if (inventorySize == 0)
+		//{
+		//    playerSpeed = 100;
+		//}
+		//else if (inventorySize == 1)
+		//{
+		//    playerSpeed = 80;
+		//}
 
-        //else if (inventorySize == 2)
+		//else if (inventorySize == 2)
 
-        Vector2 velocity = player.Velocity;
+		Vector2 velocity = player.Velocity;
 
 
-        player.Velocity = direction * playerSpeed;
-        player.MoveAndSlide();
+		player.Velocity = direction * playerSpeed;
+		player.MoveAndSlide();
 
-        if (direction.Length() > 0)
-        {
-            lastDirection = direction;
-        }
-        
-       
+		if (direction.Length() > 0)
+		{
+			lastDirection = direction;
+		}
+		
+	   
 
-    }
+	}
 }
